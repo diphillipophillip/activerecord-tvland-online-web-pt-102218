c59c4e51ee31a540643ebfeda9cc714546b3743d
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-27 01:31:03 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def character 
-    new = Character.new()
+    new = Character.new(name: "Khaleesi")
+    
   end 
 end
