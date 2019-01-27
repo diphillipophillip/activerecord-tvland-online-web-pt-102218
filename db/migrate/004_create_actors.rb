93e054898966afdc048deac1eed3f013a39665f1
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-27 01:40:03 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -4,7 +4,8 @@ class CreateActors < ActiveRecord::Migration[4.2]
   def change 
     create_table :actors do |t| 
     t.string :first_name
-    t.string :last_name 
+    t.string :last_name
+    t.integer :actor_id
    end 
   end 
 end 
